b629240f06e416f2647b9a6d2f34ea07fa70888a
Loulblemo <ghaelen.m@gmail.com>
2024-05-09 14:38:13 +0200

    Add a bit of complexity

--- app1/wsgi.py
+++ app1/wsgi.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/compute")
+def compute():
+
+    x = random.randint(0, 10)
+    y = random.randint(0, 10)
+    z = x * y
+
+    return render_template("compute.html", x=x, y=y, z=z)
 
 if __name__ == '__main__':
     app.run(debug=True)
