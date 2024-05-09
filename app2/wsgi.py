acbf1428f9c10c6fb29d018d6cec0790e021a814
Loulblemo <ghaelen.m@gmail.com>
2024-05-09 18:18:27 +0200

    Proper distinction between app.py and wsgi.py

--- app2/wsgi.py
+++ app2/wsgi.py
@@ -1,21 +1,4 @@
-import random
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/compute")
-def compute():
-
-    x = random.randint(0, 10)
-    y = random.randint(0, 10)
-    z = x + y
-
-    return render_template("compute.html", x=x, y=y, z=z)
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
